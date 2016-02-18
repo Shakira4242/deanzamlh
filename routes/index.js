fa7809a6972224fd20ac43bc47c275aec732968f
Akash Shekara <akashshekara1212@gmail.com>
2016-02-17 16:21:27 -0800

    added index.js

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('introduction',{introduction: 'Hello'});
+  res.render('index');
 });
 
 router.get('/homepage', function(req, res, next) {
-  res.render('homepage',{introduction: 'Hello'});
+  res.render('homepage');
 });
 
 

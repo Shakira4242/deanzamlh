df253f20ae56f2600a969b67bd6d9d45394ebab0
Akash Shekara <akashshekara1212@gmail.com>
2016-02-17 20:16:55 -0800

    test

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,13 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-  request('http://api.icndb.com/jokes/random', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        var chuck = JSON.parse(body); 
-        res.render('index',{chucknorrisquotes: chuck.value.joke.replace(/&quot;/g,'"')});
-    }
-	});
+  // request('http://api.icndb.com/jokes/random', function (error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //       var chuck = JSON.parse(body); 
+        // res.render('index',{chucknorrisquotes: chuck.value.joke.replace(/&quot;/g,'"')});
+ //    }
+	// });
+	res.render('index');
 });
 
 router.get('/homepage', function(req, res, next) {

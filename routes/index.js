e0f06974bd69b8f1aed4569015875f3dc6df1c6b
Akash Shekara <akashshekara1212@gmail.com>
2016-02-17 20:37:57 -0800

    make changes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
 var express = require('express');
-var request = require('request');
 var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-  // request('http://api.icndb.com/jokes/random', function (error, response, body) {
-  //   if (!error && response.statusCode == 200) {
-  //       var chuck = JSON.parse(body); 
-        // res.render('index',{chucknorrisquotes: chuck.value.joke.replace(/&quot;/g,'"')});
- //    }
-	// });
-	res.render('index');
+  request('http://api.icndb.com/jokes/random', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var chuck = JSON.parse(body); 
+        res.render('index',{chucknorrisquotes: chuck.value.joke.replace(/&quot;/g,'"')});
+    }
+	});
 });
 
 router.get('/homepage', function(req, res, next) {

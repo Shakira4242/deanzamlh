a3bf43feaa48ab148788086668f3369a710a22fa
Akash Shekara <akashshekara1212@gmail.com>
2016-02-17 19:37:35 -0800

    chuckie chuck chuck

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 var express = require('express');
-var requestify = require('requestify');
+var request = require('request');
 var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-  requestify.get("http://api.icndb.com/jokes/random")
-  	.then(function(response){
-  		console.log(response.getBody().value.joke);
-  		res.render('index',{chucknorrisquotes: response.getBody().value.joke});
-  	});
+  request('http://api.icndb.com/jokes/random', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var chuck = JSON.parse(body); 
+        res.render('index',{chucknorrisquotes: chuck.value.joke.replace(/&quot;/g,'"')});
+    }
+	});
 });
 
 router.get('/homepage', function(req, res, next) {
   res.render('homepage');
 });
-
 
 module.exports = router;

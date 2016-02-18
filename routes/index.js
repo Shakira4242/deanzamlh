3784c60763266ef7d7f2eb23c5a94bfee77dbbb6
Akash Shekara <akashshekara1212@gmail.com>
2016-02-17 17:16:50 -0800

    quote added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
-
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index');
+  request("http://api.icndb.com/jokes/random",function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	joke = JSON.parse(body);
+	    res.render('index',{chucknorrisquotes: joke.value.joke});
+	  }
+  })
 });
 
 router.get('/homepage', function(req, res, next) {
